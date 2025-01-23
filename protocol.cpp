c52985bfa90a15267674eacd6b9a818f49f73c12
Maren <31197527+maren7@users.noreply.github.com>
2025-01-23 13:31:11 +0000

    Update protocol.cpp
    bitscript params.

--- protocol.cpp
+++ protocol.cpp
@@ -24,7 +24,7 @@ static const char* ppszTypeName[] =
 
 unsigned short nDefaultP2Port = 0;
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xf8, 0xbc, 0xb3, 0xd7 };
 
 CMessageHeader::CMessageHeader()
 {
