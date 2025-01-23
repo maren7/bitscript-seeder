c6f39607fa99b7ced965ad1b1823941dbb7bb2c7
Maren <31197527+maren7@users.noreply.github.com>
2025-01-23 13:39:41 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -465,7 +465,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", "kjy2eqzk4zwi5zd3.onion", ""};
+static const string mainnet_seeds[] = {"seed.arede.site", ""};
 static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
                                        "testnet-seed.bitcoin.petertodd.org",
                                        "testnet-seed.bluematt.me",
